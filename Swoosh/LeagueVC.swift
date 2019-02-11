a47b606f536b73535d68a1255ec54470ad8bb898
Julian Mino <julianmino@Julians-Mac.local>
2019-02-11 13:03:42 -0300

    added Player struct and pass data

--- Swoosh/LeagueVC.swift
+++ Swoosh/LeagueVC.swift
@@ -9,27 +9,38 @@
 import UIKit
 
 class LeagueVC: UIViewController {
+    
+    var player: Player!
 
+    @IBOutlet weak var onNextTapped: Border_Button!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        player = Player()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    @IBAction func onMenTapped(_ sender: Any) {
+        selectleague(leagueType: "mens")
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    @IBAction func onWomenTapped(_ sender: Any) {
+        selectleague(leagueType: "womens")
     }
-    */
-
+    
+    @IBAction func onCoedTapped(_ sender: Any) {
+        selectleague(leagueType: "co-ed")
+    }
+    
+    func selectleague(leagueType: String) {
+        player.desiredLeague = leagueType
+        onNextTapped.isEnabled = true
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let skillVC = segue.destination as? SkillVC {
+            skillVC.player = player
+        }
+    }
 }
---
+++ Swoosh/Player.swift
@@ -0,0 +1,14 @@
+//
+//  Player.swift
+//  Swoosh
+//
+//  Created by Julian Mino on 2/11/19.
+//  Copyright © 2019 Julian Mino. All rights reserved.
+//
+
+import Foundation
+
+struct Player {
+    var desiredLeague: String!
+    var selectedSkill: String!
+}
